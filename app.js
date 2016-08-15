0c15429d1c96ccb9e3de9e642033884c4d6a2655
jessonfoo <jessonfoo@gmail.com>
2016-08-15 15:21:04 -0700

    gulp webserver

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var path = require('path');
 // var favicon = require('serve-favicon');
+var _= require('lodash');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
