3d7da5ae4b5f32c62a39250c0de1da1f56f6bf7f
Amanda Beaudin <amandalbeaudin@gmail.com>
2016-08-17 19:50:08 -0700

    ejs rendered on index.html page

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 var express = require('express');
 var path = require('path');
 // var favicon = require('serve-favicon');
+var _ = require('underscore');
+var ejs = require('ejs');
 var logger = require('morgan');
 var mongoose = require('mongoose');
 var authOptions = require('./config/auth_options');
@@ -46,8 +48,9 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-app.set('view engine', 'jade');
+// app.use(express.static(path.join(__dirname, 'public')));
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
 app.use('/', routes);
 
 // flickr test route
